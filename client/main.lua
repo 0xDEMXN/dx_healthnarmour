b1f623a90ca4773e85966e6037eed01bfffe686b
d3rp-jsx <15928886+d3rp-jsx@users.noreply.github.com>
2021-08-22 23:19:53 +0200

    rewrite(client): update event is now initialised inside a function

--- client/main.lua
+++ client/main.lua
@@ -1,9 +1,16 @@
-Citizen.CreateThread(function()
-  while true do
-    local playerPed = PlayerPedId()
-    TriggerServerEvent('esx_healthnarmour:update', GetEntityHealth(playerPed), GetPedArmour(playerPed))
-    Citizen.Wait(1000)
-  end
+local HealthNArmour = function()
+  Citizen.CreateThread(function()
+    while true do
+      local playerPed = PlayerPedId()
+      TriggerServerEvent('esx_healthnarmour:update', GetEntityHealth(playerPed), GetPedArmour(playerPed))
+      Citizen.Wait(1000)
+    end
+  end)
+end
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(playerId, xPlayer)
+  HealthNArmour();
 end)
 
 RegisterNetEvent('esx_healthnarmour:set')
