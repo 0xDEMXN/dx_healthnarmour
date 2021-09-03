d689e51d2f17fd3c442745125e98ec12e88ff4e9
d3rp-jsx <15928886+d3rp-jsx@users.noreply.github.com>
2021-09-03 17:53:51 +0200

    refactor: code clean-up + minor adjustments

--- client/main.lua
+++ client/main.lua
@@ -1,11 +1,6 @@
-HealthNArmour = nil
-
 RegisterNetEvent('esx_healthnarmour:set')
 AddEventHandler('esx_healthnarmour:set', function(health, armour)
     local playerPed = PlayerPedId()
-    if not HealthNArmour then
-        HealthNArmour = true
-        SetEntityHealth(playerPed, health)
-        SetPedArmour(playerPed, armour)
-    end
+    SetEntityHealth(playerPed, health)
+    SetPedArmour(playerPed, armour)
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'd3rp-jsx'
 description 'Store health and armour into your database'
-version '1.0.0'
+version '1.0.2'
 
 shared_script '@es_extended/imports.lua'
 
