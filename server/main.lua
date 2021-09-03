e7495cb05eac1b252eaa28398b66bbd6402a4f21
SavageLx <72369074+savagefromlx@users.noreply.github.com>
2021-09-03 14:16:29 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,26 +1,26 @@
 Citizen.CreateThread(function()
-    MySQL.Async.store('SELECT `health` FROM `users` WHERE `identifier` = ?', function(storeId) LoadHealthQuery = storeId end)
-    MySQL.Async.store("UPDATE `users` SET `health` = ? WHERE `identifier` = ?", function(storeId) UpdateHealthQuery = storeId end)
+    MySQL.Async.store('SELECT `health`, `armour` FROM `users` WHERE `identifier` = ?', function(storeId) LoadHealthNArmour = storeId end)
+    MySQL.Async.store("UPDATE `users` SET `health` = ?, `armour` = ? WHERE `identifier` = ?", function(storeId) UpdateHealthNArmour = storeId end)
 end)
 
 RegisterNetEvent('esx:onPlayerSpawn')
-AddEventHandler('esx:onPlayerSpawn', function() -- load
+AddEventHandler('esx:onPlayerSpawn', function()
     local playerId = source
     local xPlayer = ESX.GetPlayerFromId(playerId)
     if xPlayer ~= nil then
-        MySQL.Async.fetchAll(LoadHealthQuery, {xPlayer.identifier}, function(data)
-            if data[1].health ~= nil then
-                TriggerClientEvent('sv_player:setHealth', playerId, data[1].health)
+        MySQL.Async.fetchAll(LoadHealthNArmour, {xPlayer.identifier}, function(data)
+            if data[1].health ~= nil and data[1].armour ~= nil then
+              TriggerClientEvent('esx_healthnarmour:set', playerId, data[1].health, data[1].armour)
             end
         end)
     end
 end)
 
-
 AddEventHandler('esx:playerDropped', function(playerId, reason) -- update
     local xPlayer = ESX.GetPlayerFromId(playerId)
     if xPlayer ~= nil then
         local health = GetEntityHealth(GetPlayerPed(xPlayer.source))
-        MySQL.Async.execute(UpdateHealthQuery, {health, xPlayer.identifier})
+        local armour = GetPedArmour(GetPlayerPed(xPlayer.source))
+        MySQL.Sync.execute(UpdateHealthNArmour, {health, armour, xPlayer.identifier})
     end
 end)
