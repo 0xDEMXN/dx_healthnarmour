794ae6428e8afc7187e46422eb06cee97252962e
SavageLx <72369074+savagefromlx@users.noreply.github.com>
2021-09-03 14:01:20 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,32 +1,10 @@
-HealthNArmour = nil
+local applied = nil
 
-Citizen.CreateThread(function()
-  while true do
-    local sleep = 10000
-    if HealthNArmour then
-      local playerPed = PlayerPedId()
-      TriggerServerEvent('esx_healthnarmour:update', GetEntityHealth(playerPed), GetPedArmour(playerPed))
-      sleep = 1000
+RegisterNetEvent('sv_player:setHealth')
+AddEventHandler('sv_player:setHealth', function(health)
+    local playerPed = PlayerPedId()
+    if not applied then
+        applied = true
+        SetEntityHealth(playerPed, health)
     end
-    Citizen.Wait(sleep)
-  end
-end)
-
-AddEventHandler('esx:onPlayerSpawn', function()
-  Citizen.Wait(5000)
-  HealthNArmour = true
-end)
-
-RegisterNetEvent('esx_healthnarmour:set')
-AddEventHandler('esx_healthnarmour:set', function(health, armour)
-  local playerPed = PlayerPedId()
-  if not HealthNArmour then
-    SetEntityHealth(playerPed, health)
-    SetPedArmour(playerPed, armour)
-  end
-end)
-
-RegisterNetEvent('esx:onPlayerLogout')
-AddEventHandler('esx:onPlayerLogout', function()
-  HealthNArmour = false
 end)
